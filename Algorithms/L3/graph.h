241115692cd9812e5fa3c3fc2d423ee7fc316d84
SqrtMinusOne <thexcloud@gmail.com>
2018-04-12 18:33:47 +0300

    Base for graphs

---
+++ Algorithms/L3/graph.h
@@ -0,0 +1,104 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+#define SAVEITS List* t_lpos = KeepItL(); Elem* t_pos = KeepItE();
+#define RESTOREITS RestoreItL(t_lpos); RestoreItE(t_pos);
+#define Numb 80
+#define Wid 800
+#define Hei 600
+
+#include <iostream>
+#include <cstring>
+#include <QTextStream>
+#include <QString>
+#include <QFile>
+#include <QStack>
+#include <QMessageBox>
+#include <QTime>
+
+#include "graphwidget.h"
+
+using namespace std;
+
+typedef struct List List;
+
+typedef struct Elem{ //Элементы графа
+    Elem() = default;
+    char name[Numb];
+    Elem* next=nullptr;
+    List* childs=nullptr; //Дети элемента
+    Node* node;
+}Elem;
+
+typedef struct List{
+    List() = default;
+    char name[Numb];
+    bool mark = false;
+    Elem* node = nullptr; //Сам элемент
+    List* next = nullptr;
+    Edge* edge;
+}List;
+
+class Graph
+{
+public:
+    Graph();
+    ~Graph();
+    void Clear(); //Очистка
+
+    Elem* it(); //Итератор через вершины
+    List* it(Elem* el, bool marked = 1); //Итератор через сыновей
+    void ResetIts();
+
+    void ReadFile(QString fileName); //Считать из файла
+    void SaveFile(QString fileName); //Сохранить в файл
+
+    char* GetMinStupidName(); //Получение минимального числового имени
+    char* GetLastStupidName(); //Получение последнего такого
+
+    void AddElem(char* name); //Добавить элемент
+    void AddEdge(Elem* el1, Elem* el2); //Добавить линию
+    void AddEdge(Elem* el1, char* name); //Добавить "заготовку" для линии
+    bool Solve(); //Разрешить зависимости "заготовок" для линий
+    void RemoveElem(char* name); //Удалить элемент
+    void RemoveEdge(Elem* el1, Elem* el2); //Удалить линию
+    void RemoveEdges(Elem* el); //Удалить связи элемента
+    void RenameElem(char* oldname, char* newname); //Переименовать элемент
+
+    void Desorientate(); //Снять ориентацию графа
+
+    Elem* FindElem(char* name); //Найти элемент
+    int CountChildren(Elem* el, bool marked = 1); //Сколько детей
+    int CountElems(); //Сколько элементов
+
+    bool Is_Egde(Elem* el1, Elem* el2); //Есть ли связь от 1 к 2
+    List* GetEdge(Elem* el1, Elem* el2); //Обратить связь
+
+    int Max_Width(); //Максимальная длина имени в графе
+    void Inc_Matr(QTextStream& os); //Матрица инцидентности
+
+//    bool Euler(); //Один шаг поиска эйлерового цикла
+//    void ResetEuler(); //Сброс эйлеровых циклов
+    void ClearMarks(); //Очистка меток исключений на графе
+
+    Elem* operator[](int i); //Доступ к элементам по индексу
+
+    GraphWidget* widget;
+
+    QStack<Elem*> SE;
+    QStack<Elem*> Stack; //Стек, в котором вершины выделяются
+    Elem* v0 = nullptr;
+
+private:
+    int stupidnames = 0;
+    Elem* gr;
+    Elem* pos;
+    List* lpos;
+    void Clear(List* ls, Elem *el);
+    void Clear(Elem* gr);
+    Elem* KeepItE();
+    List* KeepItL();
+    void RestoreItE(Elem* t_pos);
+    void RestoreItL(List* t_lpos);
+};
+
+#endif // GRAPH_H
---
+++ Algorithms/L3/mainwindow.cpp
@@ -0,0 +1,265 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "graphwidget.h"
+
+#include <QTextStream>
+#include <QByteArray>
+#include <QInputDialog>
+#include <QFileDialog>
+#include <cstdlib>
+#include <ctime>
+#include <QTimer>
+
+#include <QGraphicsScene>
+
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    gr1 = new Graph;
+    gr1->widget = new GraphWidget(0, gr1, this);
+    ui->graphicsView->setViewport(gr1->widget);
+    srand(time(nullptr));
+    timer = nullptr;
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::update_matr()
+{
+    on_matrButton_clicked();
+}
+
+int MainWindow::get_speed()
+{
+    return ui->speedSlider->value();
+}
+
+void MainWindow::One_Step() //Один шаг алгоритма
+{
+    if (timer){
+        timer->stop();
+        delete timer;
+        timer = nullptr;
+        return;
+    }
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *event)
+{
+    gr1->widget->keyPressEvent(event); //Передача управления виджету
+}
+
+
+void MainWindow::on_exit_action_triggered()
+{
+    exit(0); //Выход из программы
+}
+
+void MainWindow::on_matrButton_clicked() //Построение матрицы
+{
+    QString matr;
+    QTextStream ms(&matr);
+    gr1->Inc_Matr(ms);
+    ui->incEdit->setPlainText(matr);
+}
+
+void MainWindow::on_addNodeButton_clicked() //Добавление вершины
+{
+    QString name = ui->NodeNameEdit->text();
+    if (name.length() > Numb){
+        QMessageBox msg;
+        msg.setText("Слишком длинное имя");
+        msg.exec();
+        ui->NodeNameEdit->clear();
+        return;
+    }
+    QByteArray arr = name.toLocal8Bit(); //Конвертация QString в char*
+    char* cname = arr.data();
+    if (!gr1->FindElem(cname))
+        gr1->AddElem(cname);
+    else{
+        QMessageBox msg;
+        msg.setText("Элемент уже существует");
+        msg.exec();
+    }
+    ui->NodeNameEdit->clear();
+    on_matrButton_clicked(); //После каждой процедуры матрица обновляется
+}
+
+void MainWindow::on_deleteNodeButton_clicked() //Удаление вершины
+{
+     QString name = ui->NodeNameEdit->text();
+     QByteArray arr = name.toLocal8Bit();
+     char* cname = arr.data();
+     if (gr1->FindElem(cname))
+         gr1->RemoveElem(cname);
+     else{
+         QMessageBox msg;
+         msg.setText("Элемент не существует");
+         msg.exec();
+     }
+     ui->NodeNameEdit->clear();
+     on_matrButton_clicked();
+}
+
+void MainWindow::on_AddEdge_clicked()
+{
+    QString name = ui->NodeNameEdit->text();
+    QByteArray arr = name.toLocal8Bit();
+    char* cname = arr.data();
+    Elem* el1 = gr1->FindElem(cname);
+    if (!el1){
+        QMessageBox msg;
+        msg.setText("Элемент 1 не существует");
+        msg.exec();
+        ui->NodeNameEdit->clear();
+        return;
+    }
+    bool ok;
+    QString name2 = QInputDialog::getText(this, "Добавить связь", "Введите имя", QLineEdit::Normal, "", &ok);
+    QByteArray arr2 = name2.toLocal8Bit();
+    char* cname2 = arr2.data();
+    Elem* el2 = gr1->FindElem(cname2);
+    if (!el2){
+        QMessageBox msg;
+        msg.setText("Элемент 2 не существует");
+        msg.exec();
+        ui->NodeNameEdit->clear();
+        return;
+    }
+    if (!gr1->Is_Egde(el1, el2)){
+        gr1->AddEdge(el1, el2);
+    }
+    else{
+        QMessageBox msg;
+        msg.setText("Связь уже есть");
+        msg.exec();
+    }
+    on_matrButton_clicked();
+    ui->NodeNameEdit->clear();
+}
+
+void MainWindow::on_DeleteEdgeButton_clicked() //Удаление ребра
+{
+    QString name = ui->NodeNameEdit->text();
+    QByteArray arr = name.toLocal8Bit();
+    char* cname = arr.data();
+    Elem* el1 = gr1->FindElem(cname);
+    if (!el1){
+        QMessageBox msg;
+        msg.setText("Элемент не существует");
+        msg.exec();
+        ui->NodeNameEdit->clear();
+        return;
+    }
+    bool ok;
+    QString name2 = QInputDialog::getText(this, "Удалить связь", "Введите имя", QLineEdit::Normal, "", &ok);
+    QByteArray arr2 = name2.toLocal8Bit();
+    char* cname2 = arr2.data();
+    Elem* el2 = gr1->FindElem(cname2);
+    if (!el2){
+        QMessageBox msg;
+        msg.setText("Элемент 2 не существует");
+        msg.exec();
+        ui->NodeNameEdit->clear();
+        return;
+    }
+    if (gr1->Is_Egde(el1, el2)){
+        gr1->RemoveEdge(el1, el2);
+    }
+    else{
+        QMessageBox msg;
+        msg.setText("Связи нет");
+        msg.exec();
+    }
+    on_matrButton_clicked();
+    ui->NodeNameEdit->clear();
+}
+
+void MainWindow::on_ClearButton_clicked() //Сброс графа
+{
+    gr1->Clear();
+    on_matrButton_clicked();
+    ui->NodeNameEdit->clear();
+    gr1->widget->update();
+}
+
+void MainWindow::on_open_action_triggered() //Открытие из файла
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "Открыть файл");
+    gr1->Clear();
+    gr1->ReadFile(fileName);
+    on_matrButton_clicked();
+}
+
+void MainWindow::on_save_action_triggered() //Сохранение в файл
+{
+    QString fileName = QFileDialog::getSaveFileName(this, "Открыть файл");
+    gr1->SaveFile(fileName);
+}
+
+void MainWindow::on_gravityBox_stateChanged(int arg1) //Изменение гравитации
+{
+    if (arg1 == 0){
+        gr1->widget->SetGravity(0);
+    }
+    else
+        gr1->widget->SetGravity(1);
+}
+
+void MainWindow::on_colorButton_clicked() //Рандомизация цветов
+{
+    gr1->widget->RandomColors();
+}
+
+void MainWindow::on_shuffleButton_clicked() //Перетасовка вершин
+{
+    gr1->widget->shuffle();
+}
+
+void MainWindow::on_pushButton_clicked() //Запуск алгоритма
+{
+    if (ui->stepBox->isChecked()) //Если пошаговое выполнение, просто выполнить один шаг
+        One_Step();
+    else if (!timer){
+        timer = new QTimer(this); //Иначе выполнение по таймеру
+        connect(timer, SIGNAL(timeout()), this, SLOT(One_Step()));
+        timer->start(ui->speedSlider->value());
+    }
+}
+
+void MainWindow::on_clrMarksButton_clicked() //Сброс алгоритма
+{
+    //gr1->ResetEuler();
+    gr1->ClearMarks();
+    ui->stackEdit->clear();
+    ui->resEdit->clear();
+    if (timer){
+        timer->stop();
+        delete timer;
+        timer = nullptr;
+    }
+
+}
+
+void MainWindow::on_desButton_clicked() //Убрать ориентацию графа
+{
+    gr1->Desorientate();
+    gr1->widget->update();
+    on_matrButton_clicked();
+}
+
+void MainWindow::on_speedSlider_sliderMoved(int position) //Изменение скорости
+{
+    if (timer){
+        timer->setInterval(position);
+    }
+}
+
