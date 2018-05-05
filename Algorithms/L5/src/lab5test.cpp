ce6bd32e5f47121975c576ad5ddd824f171100fb
SqrtMinusOne <thexcloud@gmail.com>
2018-05-05 18:22:26 +0300

    PrefixTest OK

--- Algorithms/L5/src/lab5test.cpp
+++ Algorithms/L5/src/lab5test.cpp
@@ -3,11 +3,17 @@
 #include "gtest/gtest.h"
 
 typedef struct PrefixTestType{
-    char* input;
+    const char* input;
     vector<int> output;
 }PrefixTestType;
 
 PrefixTestType PrefixTest1 = {"abab", vector<int> {0, 0, 1, 2}};
+PrefixTestType PrefixTest2 = {"abcdabscabcdabia", 
+    vector<int> {0, 0, 0, 0, 1, 2, 0, 0, 1, 2, 3, 4, 5, 6, 0, 1}};
+PrefixTestType PrefixTest3 = {"abcdabcabcdabcdab", 
+    vector<int> {0, 0, 0, 0, 1, 2, 3, 1, 2, 3, 4, 5, 6, 7, 4, 5, 6}};
+PrefixTestType PrefixTest4 = {"abcd", vector<int> {0, 0, 0, 0}};
+
 
 class PrefixTest : public ::testing::TestWithParam<PrefixTestType>{
     public:
@@ -16,7 +22,8 @@ class PrefixTest : public ::testing::TestWithParam<PrefixTestType>{
 
 };
 
-INSTANTIATE_TEST_CASE_P(PrefixTestInstantiation, PrefixTest, ::testing::Values(PrefixTest1));
+INSTANTIATE_TEST_CASE_P(PrefixTestInstantiation, PrefixTest, ::testing::Values(PrefixTest1, 
+    PrefixTest2, PrefixTest3, PrefixTest4));
 
 TEST_P(PrefixTest, PrefixTestTrue)
 {
