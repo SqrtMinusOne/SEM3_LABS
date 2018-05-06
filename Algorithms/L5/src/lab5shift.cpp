9db2f98e3bd95a24fabe5eee481f6d59f4caba48
SqrtMinusOne <thexcloud@gmail.com>
2018-05-06 21:46:22 +0300

    One more file

---
+++ Algorithms/L5/src/lab5shift.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+#include "kmp.h"
+
+using namespace std;
+int main(int argc, char *argv[])
+{
+    string A; //Строка 1
+    string B; //Строка 2
+    cin >> B;
+    cin >> A;
+    vector<int> res = kmp(A, B + B);
+    int n = res.size();
+    if (n > 0){
+        cout << res[0];
+    }
+    else
+        cout << "-1";
+    cout << endl;
+    return 0;
+}
