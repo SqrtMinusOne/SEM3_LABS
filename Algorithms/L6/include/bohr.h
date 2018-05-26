6bf38643f69c419dad3635c7725e1ceb1460d709
SqrtMinusOne <thexcloud@gmail.com>
2018-05-26 16:18:17 +0300

    ./include/ahocorasick.h

--- Algorithms/L6/include/bohr.h
+++ Algorithms/L6/include/bohr.h
@@ -3,28 +3,42 @@
 #include <vector>
 #include <string>
 #include <stack>
+#include <list>
 #include <iostream>
 
 using namespace std;
 
+typedef short unsigned int char_t;
+
+struct Node;
+
+typedef struct Node_link{
+    Node* link;
+    char_t ch;
+}Node_link;
+
+typedef list<Node_link> Node_list;
+
 typedef struct Node{
-    vector<Node*> son; //Children
-    vector<Node*> go; //Transitions
+    vector<Node_list> son; //Children
+    vector<Node_list> go; //Transitions
     Node* parent;
-    char charToParent;
+    char_t charToParent;
     Node* suffLink;
     Node* up; //Compressed suffix link
     bool isLeaf;
-    vector<int> leafPatternNumber;
+    vector<int> leafPatternNumber; //test
     Node();
 }Node;
 
-char SYM(const char ch, const char jok = 0);
+char_t SYM(const char_t ch, const char_t jok = 0);
+Node* isSon(vector<Node_list> & vec, const char_t ch);
+Node* addSon(Node* v, vector<Node_list> & vec, const char_t ch);
 
-void addString(const string & s, int patternNumber, Node* root, const char jok = 0);
+void addString(const string & s, int patternNumber, Node* root, const char_t jok = 0);
 void out(Node* n);
 Node* getSuffLink(Node* v, Node* root);
-Node* getLink(Node* v, char c, Node* root);
+Node* getLink(Node* v, char_t c, Node* root);
 Node* getUp(Node* v, Node* root);
 
 #endif
